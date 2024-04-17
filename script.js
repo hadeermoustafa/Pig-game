615229e00b3d5fb1fc96160a7aa2ac42cdcbef32
Hadeer Moustafa <hadeer.moustafa@outlook.com>
2024-04-17 14:00:52 +0200

    adding js code

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+
+//select emlements
+
+const score1 = document.getElementById("score--0");
+const score2 = document.getElementById("score--1");
+const dice = document.querySelector(".dice");
+
+//setting score to 0;
+score1.textContent = 0;
+score2.textContent = 0;
+dice.classList.add("hidden");
