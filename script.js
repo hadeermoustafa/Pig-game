25609f6ead7bbb4f1268bac616c1345c631a9411
Hadeer Moustafa <hadeer.moustafa@outlook.com>
2024-04-17 16:18:50 +0200

    doing the hold score function

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 //select emlements
 const player0 = document.querySelector(".player--0");
 const player1 = document.querySelector(".player--1");
-const score1 = document.getElementById("score--0");
-const score2 = document.getElementById("score--1");
+const score0 = document.getElementById("score--0");
+const score1 = document.getElementById("score--1");
 const dice = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnHold = document.querySelector(".btn--hold");
@@ -14,11 +14,23 @@ const currentScore1 = document.getElementById("current--1");
 
 //setting score ;
 const scores = [0, 0];
+score0.textContent = 0;
 score1.textContent = 0;
-score2.textContent = 0;
 dice.classList.add("hidden");
 let score = 0;
 let activePlayer = 0;
+
+//switching players
+
+function switchingPlayers() {
+  // switch player
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  score = 0;
+  //change active or idel style
+  player0.classList.toggle("player--active");
+  player1.classList.toggle("player--active");
+}
 
 //Rolling dice
 
@@ -37,11 +49,15 @@ btnRoll.addEventListener("click", function () {
     console.log(score);
     document.getElementById(`current--${activePlayer}`).textContent = score;
   } else {
-    // switch player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    score = 0;
-    player0.classList.toggle("player--active");
-    player1.classList.toggle("player--active");
+    switchingPlayers();
   }
+});
+
+btnHold.addEventListener("click", function () {
+  //add score to active player score
+  scores[activePlayer] += score;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  //change active or idel style
+  switchingPlayers();
 });
